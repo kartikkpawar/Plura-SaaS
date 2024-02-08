c5bc0376927f0057e3980ec19c3cfc04adfc9491
kartikkpawar <knpwar789@gmail.com>
2024-02-08 18:50:12 +0530

    Agency Details -> Settings Page

--- src/app/(main)/agency/[agencyId]/settings/page.tsx
+++ src/app/(main)/agency/[agencyId]/settings/page.tsx
@@ -1,7 +1,45 @@
+import AgencyDetails from "@/components/forms/AgencyDetails";
+import UserDetails from "@/components/forms/UserDetails";
+import { db } from "@/lib/db";
+import { currentUser } from "@clerk/nextjs";
 import React from "react";
 
-const SettingsPage = () => {
-  return <div>SettingsPage</div>;
+type Props = {
+  params: { agencyId: string };
+};
+
+const SettingsPage = async ({ params }: Props) => {
+  const authUser = await currentUser();
+  if (!authUser) return null;
+
+  const userDetails = await db.user.findUnique({
+    where: { email: authUser.emailAddresses[0].emailAddress },
+  });
+
+  if (!userDetails) return null;
+
+  const agencyDetails = await db.agency.findUnique({
+    where: { id: params.agencyId },
+    include: {
+      SubAccount: true,
+    },
+  });
+
+  if (!agencyDetails) return null;
+
+  const subAccounts = agencyDetails.SubAccount;
+
+  return (
+    <div className="flex lg:!flex-row flex-col gap-4">
+      <AgencyDetails data={agencyDetails} />
+      <UserDetails
+        type="agency"
+        id={params.agencyId}
+        subAccounts={subAccounts}
+        userData={userDetails}
+      />
+    </div>
+  );
 };
 
 export default SettingsPage;
