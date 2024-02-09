e7b1a1b77d9c2bdb5813bd51a22c1e57c8612c30
kartikkpawar <knpwar789@gmail.com>
2024-02-09 21:14:16 +0530

    teams page

--- src/app/(main)/agency/[agencyId]/team/page.tsx
+++ src/app/(main)/agency/[agencyId]/team/page.tsx
@@ -1,7 +1,53 @@
-import React from "react";
+import { db } from "@/lib/db";
+import { currentUser } from "@clerk/nextjs";
+import React, { Fragment } from "react";
+import DataTable from "./DataTable";
+import { PlusIcon } from "lucide-react";
+import { columns } from "./Columns";
 
-const TeamPage = () => {
-  return <div>TeamPage</div>;
+type Props = {
+  params: { agencyId: string };
+};
+
+const TeamPage = async ({ params }: Props) => {
+  const authUser = await currentUser();
+
+  const teamMembers = await db.user.findMany({
+    where: {
+      Agency: {
+        id: params.agencyId,
+      },
+    },
+    include: {
+      Agency: { include: { SubAccount: true } },
+      Permissions: { include: { SubAccount: true } },
+    },
+  });
+  if (!authUser) return;
+
+  const agencyDetails = await db.agency.findUnique({
+    where: {
+      id: params.agencyId,
+    },
+    include: { SubAccount: true },
+  });
+
+  if (!agencyDetails) return;
+
+  return (
+    <DataTable
+      actionButtonText={
+        <Fragment>
+          <PlusIcon size={15} />
+          Add
+        </Fragment>
+      }
+      modalChildren={<></>}
+      filterValue="name"
+      columns={columns}
+      data={teamMembers}
+    />
+  );
 };
 
 export default TeamPage;
