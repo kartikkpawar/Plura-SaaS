c5f32ce84a13192fc1e2eb5b7fd26b211a2983df
kartikkpawar <knpwar789@gmail.com>
2024-02-10 12:16:30 +0530

    updateLanesOrder, updateTicketsOrder queries

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -3,7 +3,15 @@
 import { clerkClient, currentUser } from "@clerk/nextjs";
 import { db } from "./db";
 import { redirect } from "next/navigation";
-import { Agency, Prisma, Role, SubAccount, User } from "@prisma/client";
+import {
+  Agency,
+  Lane,
+  Prisma,
+  Role,
+  SubAccount,
+  Ticket,
+  User,
+} from "@prisma/client";
 import { v4 } from "uuid";
 import { CreateFunnelFormSchema, CreateMediaType } from "./type";
 import { z } from "zod";
@@ -590,4 +598,44 @@ export const deletePipeline = async (pipelineId: string) => {
     where: { id: pipelineId },
   });
   return response;
+};
+export const updateLanesOrder = async (lanes: Lane[]) => {
+  try {
+    const updateTrans = lanes.map((lane) =>
+      db.lane.update({
+        where: {
+          id: lane.id,
+        },
+        data: {
+          order: lane.order,
+        },
+      })
+    );
+
+    await db.$transaction(updateTrans);
+    console.log("Done reordered 🟢");
+  } catch (error) {
+    console.log(error, "ERROR UPDATE LANES ORDER");
+  }
+};
+
+export const updateTicketsOrder = async (tickets: Ticket[]) => {
+  try {
+    const updateTrans = tickets.map((ticket) =>
+      db.ticket.update({
+        where: {
+          id: ticket.id,
+        },
+        data: {
+          order: ticket.order,
+          laneId: ticket.laneId,
+        },
+      })
+    );
+
+    await db.$transaction(updateTrans);
+    console.log("Done reordered 🟢");
+  } catch (error) {
+    console.log(error, "ERROR UPDATE TICKET ORDER 🔴");
+  }
 };
